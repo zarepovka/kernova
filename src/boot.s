049e7e4b2065ea0127cbef3b474f3ad59c905201
zarepovka <tatarin_1005@icloud.com>
2026-09-16 12:04:02 +0300

    Restore kernel source contents

--- src/boot.s
+++ src/boot.s
@@ -1,1 +1,14 @@
+.section .multiboot
+.align 4
+.long 0x1BADB002        # magic number
+.long 0x00              # flags
+.long -(0x1BADB002)     # checksum
 
+.section .text
+.global _start
+.extern kernel_main
+
+_start:
+    call kernel_main
+    cli
+    hlt
--- src/kernel.c
+++ src/kernel.c
@@ -1,1 +1,11 @@
+void kernel_main() {
+    const char *message = "Kernova: ядро загружено!\n";
+    char *video_memory = (char *) 0xb8000;
 
+    for (int i = 0; message[i] != '\0'; i++) {
+        video_memory[i * 2] = message[i];
+        video_memory[i * 2 + 1] = 0x0F;
+    }
+
+    while (1);
+}
